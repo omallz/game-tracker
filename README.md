d53bc9539e18bcdea9674662b74957a538dad62b
Jason <omallz@gmail.com>
2024-07-21 00:18:40 +0930

    Big update
    Re-worked the structure and have some decent content coming in.
    Prior to IGDB and separating pages out to reduce API calls.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # game-tracker
 Track my gaming library
+
+separate out the pages and bring them in via JS
+home page will allow adding a game to the sheet with certain required fields
+games page will then load the list of games.
+Keeping these separate to reduce the API calls.
--- script.js
+++ script.js
@@ -1,17 +1,70 @@
 function displayData(data) {
     const container = document.getElementById('data-container');
     container.innerHTML = ''; // Clear previous data
+    container.classList.add('row');
+    container.classList.add('g-3');
+    container.classList.add('row-cols-2');
+    container.classList.add('row-cols-md-4');
 
     data.forEach(item => {
-    const div = document.createElement('div');
-    div.classList.add('game-item');
-    div.textContent = `Game: ${item.Title}, Genre: ${item.Genre}`;
-    container.appendChild(div);
+
+        const gameCol = document.createElement('div');
+        gameCol.classList.add('col');
+
+        const gameCard = document.createElement('div');
+        gameCard.classList.add('card', 'h-100');
+
+        const gameCardBody = document.createElement('div');
+        gameCardBody.classList.add('card-body');
+
+        /* image */
+        const img = document.createElement('img');
+        img.src = 'https://picsum.photos/200/300'; // Placeholder image URL
+        img.alt = `${item.gameTitle} cover image`;
+        img.classList.add('card-img-top','game-image');
+
+        /* game title */
+        const gameCardTitle = document.createElement('h5');
+        gameCardTitle.classList.add('card-title');
+        gameCardTitle.textContent = `${item.gameTitle}`;
+
+        /* added to gamepass */
+        const gameAddedDate = document.createElement('p');
+        gameAddedDate.classList.add('card-text');
+        const date = new Date(item.gamepassAddDate + '-01'); // Add day to create a valid date
+        const formattedDate = date.toLocaleString('en-US', { month: 'long', year: 'numeric' });
+        gameAddedDate.innerHTML = `<i class="fa-regular fa-calendar-plus fa-fw"></i> ${formattedDate}`;
+
+        // Calculate the difference in months
+        const currentDate = new Date();
+        const diffTime = currentDate - date;
+        const diffMonths = diffTime / (1000 * 60 * 60 * 24 * 30);
+
+        // Apply background colors based on the difference in months
+        if (diffMonths >= 10) {
+            gameCard.style.backgroundColor = '#f3a7ae'; // 10 months or more
+        } else if (diffMonths >= 6) {
+            gameCard.style.backgroundColor = '#fff3cd'; // 6 months or more
+        } else {
+            gameCard.style.backgroundColor = '#bcd7ca'; // recent
+        }
+
+        /* Add the card, image and card-body */
+        gameCol.appendChild(gameCard);
+        gameCard.appendChild(img);
+        gameCard.appendChild(gameCardBody);
+
+        /* Add the content of card-body */
+        gameCardBody.appendChild(gameCardTitle);
+        gameCardBody.appendChild(gameAddedDate);
+        container.appendChild(gameCol);
+
     });
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://sheetdb.io/api/v1/x1xfo8ngykrhs?sheet=Backlog')
+    fetch('https://sheetdb.io/api/v1/x1xfo8ngykrhs?sheet=Backlog&sort_by=gamepassAddDate&sort_order=desc&sort_method=date&sort_date_format=Y-m')
     .then(response => response.json())
     .then(data => {
         displayData(data);
