7a5dda9164aeb6bd6b1933c1e99903679249f1f3
Jason <omallz@gmail.com>
2024-08-19 21:32:49 +0930

    add encoding for title search via api

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ async function fetchSheetdbData() {
 // Function to fetch game data from IGDB with pagination
 async function fetchIgdbData(gameTitle) {
     try {
-        const response = await fetch(`https://gaming-backlog-proxy-server-abba15e1c367.herokuapp.com/proxy.php?api=igdb&search=${encodeURIComponent(gameTitle)}`);
+        const encodedTitle = encodeURIComponent(gameTitle);
+        const response = await fetch(`https://gaming-backlog-proxy-server-abba15e1c367.herokuapp.com/proxy.php?api=igdb&search=${encodedTitle}`);
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
